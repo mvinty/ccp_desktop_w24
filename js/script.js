10d2edd3b3e79aa8811eac034fb064529163dbe5
vinht <vinht@umich.edu>
2024-04-07 23:08:34 -0400

    bruh

--- js/script.js
+++ js/script.js
@@ -1,3 +1,23 @@
-document.querySelector("#java").addEventListener("click", function() {
-    document.querySelector("#java").textContent = "Go Blue!"
+var totalClicks = 0;
+
+
+var shown = false;
+document.querySelector("main").addEventListener("click", function() {
+
+    totalClicks += 1;
+    localStorage.setItem("totalClicks", totalClicks);
+    if(shown) {
+        document.querySelector("#java").textContent = "Go Blue! Total clicks:" + totalClicks;
+    }
 });
+
+document.querySelector("#java").addEventListener("click", function() {
+    document.querySelector("#java").textContent = "Go Blue! Total clicks:" + totalClicks;
+    shown = true;
+});
+
+window.onload = function() {
+    //initialize total clicks from local
+    totalClicks = parseInt(localStorage.getItem("totalClicks"));
+};
+
